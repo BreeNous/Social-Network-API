7495e7bf8251fa6757cde6c3973700e8b064ae0a
BreeNous <breeanna.nouskhajian@gmail.com>
2024-05-30 15:17:50 -0400

    added the routing for user-routes.js

--- Develop/routes/api/thought-routes.js
+++ Develop/routes/api/thought-routes.js
@@ -17,19 +17,16 @@ router.route('/')
   .post(createThought);
 
 // /api/thoughts/:thoughtId
-
 router.route('/:thoughtId')
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-
 router.route('/:thoughtId/reactions')
   .post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-
 router.route('/:thoughtId/reactions/:reactionId')
 .delete(removeReaction);
 
